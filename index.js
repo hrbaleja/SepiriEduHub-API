13c84aed21cd76ed868b109a553ee901d61e5355
Hitesh Baleja <hiteshbaleja@gmail.com>
2026-02-15 18:21:30 +0530

    Update URL

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 // Body parser
 app.use(express.json({ limit: '10mb' }));
